35c2ccf368352217cfef820137fb8e6cd70f7012
jybin96 <wjddudqls96@gmail.com>
2023-07-07 15:33:51 +0900

    jacoco

--- exeption/src/test/java/com/example/exeption/controller/HomeControllerTest.java
+++ exeption/src/test/java/com/example/exeption/controller/HomeControllerTest.java
@@ -9,12 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("홈 컨트롤러 단위 테스트")
 @SpringBootTest
@@ -34,12 +36,12 @@ class HomeControllerTest {
     @DisplayName("멤버 추가 API 테스트")
     void create() throws Exception {
         String content = objectMapper.writeValueAsString(
-                new MemberRegisterDto.Request("이름입니다", "email@email.com", 14, 2000));
+                new MemberRegisterDto.Request("이름입니다", "email@email.com", 18, 2000));
 
         mvc.perform(post("/member")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(content))
-                .andExpect(jsonPath("$.code").value("C001"))
+                .andExpect(status().isOk())
                 .andDo(print());
     }
 }
